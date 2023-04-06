e9e0e0d1878c2bf6d3e1104d255ecf194d19dc37
UNDEAD-B0T <130006631+UNDEAD-B0T@users.noreply.github.com>
2023-04-06 03:31:13 -0400

    Update CollapsibleModernChatConfig.java

--- src/main/java/com/CollapsibleModernChat/CollapsibleModernChatConfig.java
+++ src/main/java/com/CollapsibleModernChat/CollapsibleModernChatConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup("CollapsibleModernChat")
 public interface CollapsibleModernChatConfig extends Config
 {
 
